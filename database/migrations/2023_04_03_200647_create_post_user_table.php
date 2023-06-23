6143c972cf92a1ea13d78c249aa481f119cf7cb3
Abdalrahman Shayah <abdulrahman.shayah@raizer.tech>
2023-06-23 21:25:41 +0300

    Fix a ; bug

--- database/migrations/2023_04_03_200647_create_post_user_table.php
+++ database/migrations/2023_04_03_200647_create_post_user_table.php
@@ -24,13 +24,13 @@ return new class extends Migration
             $table->foreign('post_id')
                 ->references('id')
                 ->on('posts')      
-                ->onDelete('cascade');
+                ->onDelete('cascade')
                 ->onUpdate('cascade');
             
             $table->foreign('operation_id')
                 ->references('id')
                 ->on('operations')
-                ->onDelete('cascade');
+                ->onDelete('cascade')
                 ->onUpdate('cascade');
 
             $table->foreign('user_id')
